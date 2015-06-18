975345253ee5b34fe85ad9183e1a89a87d91d665
neal_tsai <neal_tsai@hotmail.com>
2015-06-18 14:50:34 +0800

    Add HomeWork07
    所有整數和或所有奇數和

---
+++ HomeWork07/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork07
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string mode = SelectSumFunction();
+            int number = GetNumberInput();
+            SumNumber(number, mode);
+        }
+
+        public static string SelectSumFunction()
+        {
+            Console.WriteLine("Please select Sum Function(odd or normal)");
+            return Console.ReadLine();
+        }
+
+        public static int GetNumberInput()
+        {
+            Console.WriteLine("Please enter Value(x)");
+            return int.Parse(Console.ReadLine());
+        }
+
+        public static void SumNumber(int number, string mode)
+        {
+            int sumNumber = 0;
+
+            switch (mode)
+            {
+                case "odd":
+                    for (int i = 1; i <= number; i += 2)
+                        sumNumber += i;
+                    Console.WriteLine("{0}", sumNumber);
+                    break;
+
+                case "normal":
+                    for (int i = 1; i <= number; i++)
+                        sumNumber += i;
+                    Console.WriteLine("{0}", sumNumber);
+                    break;
+
+                default:
+                    Console.WriteLine("Select wrong mode!!! Please try again.");
+                    break;
+            }
+        }
+    }
+}
